5403ce9aacca14c4a09afc5a3c166492612de915
Timothée Lottaz <timozattol@gmail.com>
2015-02-25 09:52:19 +0100

    Add eclipse stuff to gitignore

---
+++ ix-lab02/test/ix/lab02/degdist/DegreeDistributionReducerTest.java
@@ -0,0 +1,32 @@
+package ix.lab02.degdist;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
+import org.junit.Test;
+
+/**
+ * This class contains tests to check that the DegreeDistributionReducerTest works
+ * correctly
+ */
+public class DegreeDistributionReducerTest {
+
+    @Test
+    public void latestTest() throws IOException {
+        IntWritable inputKey = new IntWritable(1);
+        List<IntWritable> inputValues = Arrays.asList(new IntWritable(2),
+                new IntWritable(1), new IntWritable(1));
+
+        IntWritable outputValue = new IntWritable(4);
+
+        new ReduceDriver<IntWritable, IntWritable, IntWritable, IntWritable>()
+                .withReducer(new DegreeDistributionReducer())
+                .withInput(inputKey, inputValues)
+                .withOutput(inputKey, outputValue)
+                .runTest();
+    }
+
+}
