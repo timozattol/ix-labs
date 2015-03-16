a22272345142809f654381ac4d46fe38ae6c86ab
Timothée Lottaz <timozattol@gmail.com>
2015-03-16 19:27:27 +0100

    Create README and implement FacebookSampling

--- ix-lab03/src/ix/lab03/sampling/FacebookSampling.java
+++ ix-lab03/src/ix/lab03/sampling/FacebookSampling.java
@@ -1,8 +1,11 @@
 package ix.lab03.sampling;
 
-import ix.utils.SocialAPI;
+import java.util.List;
+import java.util.Random;
 
-@SuppressWarnings("unused")
+import ix.utils.SocialAPI;
+import ix.utils.SocialNode;
+
 public class FacebookSampling {
 
     public static int N = 10000;
@@ -11,7 +14,32 @@ public class FacebookSampling {
     public static void main(String[] args) {
         SocialAPI api = new SocialAPI();
 
-        // TODO Complete.
-        // Hint: you might find java.util.Random useful.
+        int i = 0;
+        double ageSum = 0;
+        double inverseDegreeSum = 0;
+        SocialNode currentNode = api.getNode(GRAPH, SocialAPI.SEED_U);
+        List<String> currentNeighbors;
+        Random r = new Random();
+
+
+        while (i < N) {
+
+        	currentNeighbors = currentNode.neighbors;
+        	double numNeighbours = currentNeighbors.size();
+
+        	ageSum += currentNode.age / numNeighbours;
+
+        	inverseDegreeSum += 1 / numNeighbours;
+
+        	currentNode = api.getNode(GRAPH, currentNeighbors.get(r.nextInt((int)numNeighbours)));
+
+        	if(i % 200 == 0) {
+        		System.out.println(ageSum / inverseDegreeSum);
+        	}
+
+        	++i;
+        }
+
+        System.out.println(ageSum / inverseDegreeSum);
     }
 }
